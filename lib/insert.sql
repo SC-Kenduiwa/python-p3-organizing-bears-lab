a8ef34bc060261fec658e2315fb54d812c0a5567
SC-Kenduiwa <kenduiwashanice64@gmail.com>
2024-06-04 15:31:57 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', true),
+('Rowdy', 3, 'M', 'Black', 'Energetic', true),
+('Tabitha', 8, 'F', 'Gray', 'Calm', true),
+('Sergeant Brown', 6, 'M', 'Brown', 'Grumpy', true),
+('Melissa', 4, 'F', 'White', 'Playful', true),
+('Grinch', 10, 'M', 'Green', 'Sneaky', true),
+('Wendy', 7, 'F', 'Brown', 'Shy', true),
+(NULL, NULL, NULL, NULL, NULL, NULL); -- For the unnamed bear with null values
